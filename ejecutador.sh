175b3a19d83dfca8369b25ee8cf2d9c843e20c26
osaadn <osaadn@gmail.com>
2024-04-08 21:42:13 -0600

    Ultima modificacion

---
+++ ejecutador.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Este script pregunta por todos los paises
+
+# sacar todos los paises posibles
+todos=$(cut -d "," -f1 paises.txt | tail -n+2)
+
+for mivariable in $todos
+do
+	echo "Preguntando por $mivariable"
+	bash pregunton.sh $mivariable
+done
+
